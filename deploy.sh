74213dacaae13bd08a1630a93d24fea09ef239f9
Alexander Obi <alexander.obi@smartrac-group.com>
2017-10-23 11:48:41 -0400

    update build

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ set -e
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 docker push alexebube/nk-rest-api
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment nk-rest -p '{"spec":{"template":{"spec":{"containers":[{"name":"nk-rest","image":"alexebube/nk-rest-api:latest"}]}}}}'
+kubectl patch deployment nk-rest -p '{"spec":{"template":{"spec":{"containers":[{"name":"nk-rest","image":"alexebube/nk-rest-api:'"$CIRCLE_SHA1"'"}]}}}}'
